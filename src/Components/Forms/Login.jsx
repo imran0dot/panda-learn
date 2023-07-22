af70d461159e79afd399f8d8a99f2e4366c32185
detectiveseo <detectiveseo1@gmail.com>
2023-07-22 12:45:25 +0600

    react hoock form added

--- src/Components/Forms/Login.jsx
+++ src/Components/Forms/Login.jsx
@@ -1,12 +1,23 @@
 import { BsEyeSlashFill, BsEyeFill } from 'react-icons/bs'
+import { useForm } from "react-hook-form";
 
 const Login = ({ passwordType, setPasswordType }) => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    const onSubmit = data => console.log(data);
     return (
-        <form className='flex flex-col gap-4'>
+        <form 
+        onSubmit={handleSubmit(onSubmit)}
+        className='flex flex-col gap-4'>
             <label>
                 <p>Name</p>
-                <input type="text" placeholder="Enter Here your name" className="input input-bordered w-full" />
+                <input
+                    placeholder="Enter Here your name"
+                    className="input input-bordered w-full"
+                    {...register("name", { required: true })} />
+                {errors.name && <span className='text-red-800'>This field is required</span>}
             </label>
+
             <label>
                 <div className='flex justify-between'>
                     <p>Password</p>
@@ -16,12 +27,17 @@ const Login = ({ passwordType, setPasswordType }) => {
                             <BsEyeFill onClick={() => setPasswordType(!passwordType)} />
                     }
                 </div>
-                <input type={passwordType? "text" : "password"}  placeholder="Enter Here your Password" className="input input-bordered w-full" />
+                <input 
+                type={passwordType ? "text" : "password"} 
+                placeholder="Enter Here your Password" 
+                className="input input-bordered w-full"
+                {...register("password", { required: true })} />
             </label>
             <input
+
                 className='btn bg-[#00988A] text-white hover:bg-[#2E836F]'
                 type="submit" value="Login in" />
-        </form>
+        </form >
     );
 };
 
--- src/Components/Forms/Registration.jsx
+++ src/Components/Forms/Registration.jsx
@@ -1,16 +1,35 @@
 import { BsEyeSlashFill, BsEyeFill } from 'react-icons/bs'
+import { useForm } from "react-hook-form";
+
 
 const Registration = ({ passwordType, setPasswordType }) => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    const onSubmit = data => console.log(data);
+
     return (
-        <form className='flex flex-col gap-4'>
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className='flex flex-col gap-4'>
+            {errors.lastName && <span className='text-red-800'>This field is required</span>}
+
             <label>
                 <p>Name</p>
-                <input type="text" placeholder="Enter Here your name" className="input input-bordered w-full" />
+                <input
+                    placeholder="Enter Here your name"
+                    className="input input-bordered w-full"
+                    {...register("name", { required: true })} />
+                {errors.name && <span className='text-red-800'>This field is required</span>}
             </label>
 
             <label>
                 <p>Email</p>
-                <input type="email" placeholder="Enter Here your Email" className="input input-bordered w-full" />
+                <input
+                    type='email'
+                    placeholder="Enter Here your email"
+                    className="input input-bordered w-full"
+                    {...register("email", { required: true })} />
+                {errors.email && <span className='text-red-800'>This field is required</span>}
             </label>
 
 
@@ -23,7 +42,17 @@ const Registration = ({ passwordType, setPasswordType }) => {
                             <BsEyeFill onClick={() => setPasswordType(!passwordType)} />
                     }
                 </div>
-                <input type={passwordType? "text" : "password"} placeholder="Enter Here your Password" className="input input-bordered w-full" />
+                <input
+                    type={passwordType ?
+                        "text" : "password"}
+                    placeholder="Enter Here your Password"
+                    className="input input-bordered w-full"
+                    {...register("password", {
+                        required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/i
+                    })} />
+                {errors.password && <span className='text-red-800'>
+                Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one number, and one special character $ ! % * ? &
+                </span>}
             </label>
 
             <input
